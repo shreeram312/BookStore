6e1303430670ffccc974abf63aaacfc63f2b341f
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-03-23 23:46:08 +0530

    CRUD Operations

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,21 @@ app.post("/books", async (req, res) => {
         msg: "No Proper Inputs",
       });
     }
+
+    const existingUser = await Book.findOne({ title, author });
+    if (existingUser) {
+      return res.json({
+        msg: "Book Already Exists",
+      });
+    }
+
     const BookUser = await Book.create({
       title,
       author,
       publishYear,
     });
-    console.log(BookUser);
+
+    // console.log(BookUser);
     res.json({
       BookUser,
     });
@@ -37,8 +46,67 @@ app.post("/books", async (req, res) => {
 app.get("/getbooks", async (req, res) => {
   const books = await Book.find();
   res.json({
+    count: books.length,
     msg: books,
   });
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bookId = await Book.findById(id);
+    // console.log(bookId);
+    res.json({
+      bookId,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+    if (!title || !author || !publishYear) {
+      return res.status(403).json({
+        msg: "Send proper inputs",
+      });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+    console.log(result);
+
+    if (!result) {
+      return res.json({
+        msg: "No Book Found",
+      });
+    }
+    res.json({
+      msg: "Book Updated Succesfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteBook = await Book.findByIdAndDelete(id);
+
+    if (!deleteBook) {
+      return res.json({
+        msg: "No Book Available",
+      });
+    }
+    res.json({
+      msg: "Book Deleted Succesfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(PORT, function () {
