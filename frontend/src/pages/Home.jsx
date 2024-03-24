a46637f2e42884afc12632e7c5f552f3287f3ad9
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-03-25 00:05:10 +0530

    Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,16 +4,27 @@ import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox MdOutlineDelete} from "react-icons/md";
-
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 const Home = () => {
-    const [books,setbooks] = useState([])
-    const [loading,setLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
-        setLoading(true)
-    })
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:3000/books")
+      .then((response) => {
+        setBooks(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        // Handle errors here bro
+        console.error("Error fetching data: ", error);
+        setLoading(false);
+      });
+  }, []);
+
   return <div>hello world bro </div>;
 };
 
