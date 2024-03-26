4d6029b3e7f2f076eebc5313d3d6153703de988e
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-03-26 23:45:25 +0530

    Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,9 +13,9 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:3000/books")
+      .get("http://localhost:3000/books/getBooks")
       .then((response) => {
-        setBooks(response.data);
+        setBooks(response.data.msg);
         setLoading(false);
       })
       .catch((error) => {
@@ -25,7 +25,75 @@ const Home = () => {
       });
   }, []);
 
-  return <div>hello world bro </div>;
+  return (
+    <div className="p-4">
+      BookStore
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">Books List</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className="w-full  border-seperate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="border  border-slate-600 rounded-md">No</th>
+              <th className="border  border-slate-600 rounded-md">Title</th>
+              <th className="border  border-slate-600 rounded-md  max-md:hidden">
+                Author{""}
+              </th>
+              <th className="border  border-slate-600 rounded-md max-md:hidden">
+                Publish Year
+              </th>
+              <th className="border  border-slate-600 rounded-md">
+                Operations
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr className="h-8" key={book._id}>
+                <td className="border border-slate-700 rounded-md text-center">
+                  {index + 1}
+                </td>
+
+                <td className="border border-slate-700 rounded-md text-center">
+                  {book.title}
+                </td>
+
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  {book.author}
+                </td>
+
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  {book.publishYear}
+                </td>
+
+                <td className="border border-slate-700 rounded-md text-center">
+                  <div className="flex justify-center gap-x-4">
+                    <Link to={`book/details/${book._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-600" />
+                    </Link>
+
+                    <Link to={`book/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
+                    </Link>
+
+                    <Link to={`book/delete/${book._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-600" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default Home;
