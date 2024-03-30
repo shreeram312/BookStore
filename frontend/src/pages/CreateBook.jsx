bfbf8640ebaa6fdd75a89aea5be473edb4c6779a
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-03-31 00:01:28 +0530

    Frontend

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { Axios } from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CreateBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+
   return <div></div>;
 };
 
