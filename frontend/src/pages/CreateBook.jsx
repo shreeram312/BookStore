ff4a544cebf1410e8aba0916e478e9dceb825028
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-04-01 23:46:05 +0530

    Frontend

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -9,8 +9,33 @@ const CreateBook = () => {
   const [author, setAuthor] = useState("");
   const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  return <div></div>;
+  const handlesavebook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+
+    axios.post("http://localhost:3000/books/addBook", data).then(() => {
+      console.log("sent data ");
+      setLoading(false);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="p-4 ">
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        Title
+      </div>
+    </div>
+  );
 };
 
 export default CreateBook;
