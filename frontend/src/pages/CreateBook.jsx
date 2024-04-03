badfe450bae11b72ab3ffceb18f3de23f5ab6ba7
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-04-03 23:47:40 +0530

    frontend

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
-import { Axios } from "axios";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const CreateBook = () => {
@@ -32,7 +32,39 @@ const CreateBook = () => {
       <h1 className="text-3xl my-4">Create Book</h1>
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-        Title
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 ">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 ">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 ">PublishYear</label>
+          <input
+            type="text"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <button className="p-2 bg-sky-600 m-8" onClick={handlesavebook}>
+          Create Book
+        </button>
       </div>
     </div>
   );
